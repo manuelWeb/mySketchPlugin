75740e04bbdd3204b927e86dfbc78814add85620
manuelWeb <devries.webdesigner@gmail.com>
2018-06-03 13:03:43 +0200

    mv sum log in if layer.class() == "MSSliceLayer" statment

--- MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
+++ MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
@@ -6,33 +6,38 @@ var onRun = function(context) {
   for (var i = 0; i < pages.count(); i++){
     var page = pages[i];
     // log([page name])
+    var artboards = [page artboards];
+    // log(artboards)
+
+    for (var z = 0; z < artboards.count(); z++){
+      var artboard = artboards[z];
+      var layers = [artboard layers];
+    }
+
     // log slices :
-    var addLyW = []
+    var arySlice = []
     var sum = 0;
     for(var k = 0; k < layers.count(); k++){
       var layer = layers[k];
       if(layer.class() == "MSSliceLayer"){
-        //do something
-        // log( layers[k] )
-        // log( layer )
-        // log( layers[k].frame() )
         var layName = [layer name]
         var layClass = layer.frame()
         var layW = layClass.width()
         var layY = layClass.y()
         if (layW === 620){
           log(`layer: ${layName} vaut une slice `)
+          arySlice.push(layName)
         }else if(layW <= 620){
           log( `layer: ${layName} w:${layW} y:${layY} ` )
           sum += layW
         }
+        log(`sum: ${sum}`)
       }
-      log(`sum: ${sum}`)
       // while (sum <= 620) {
       //   continue
       // }
     }
-    log(addLyW)
+    log(arySlice)
   }
 
 }
