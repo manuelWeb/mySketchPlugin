2240a0c4aff1ce17a228550dc288d6a1d4dc68fa
manuelWeb <devries.webdesigner@gmail.com>
2018-06-03 11:27:01 +0200

    dev plugin like FW HTML output

--- MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
+++ MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
@@ -25,8 +25,38 @@ var onRun = function(context) {
       var artboard = artboards[z];
       var layers = [artboard layers];
     }
+    
+    // log slices :
+    for(var k = 0; k < layers.count(); k++){
+      var layer = layers[k];
+      if(layer.class() == "MSSliceLayer"){
+        //do something
+        // log( layers[k] )
+        // log( layer )
+        // log( layers[k].frame() )
+        var layClass = layer.frame()
+        var layW = layClass.width()
+        var layY = layClass.y()
+        var acc;
+        var addLyW = []
+        if (layW === 620){
+          log(`layer: ${[layer name]} vaut une slice `)
+        }else if(layW <= 620){
+          log( `layer: ${[layer name]} w:${layW} y:${layY} ` )
+          // log(`layer: ${layer} est à additionner pour arriver jusqu'à 620 `)
+          // log(layW)
+          // while(layer){
+            //   addLyW.push(layW)
+            // }
+          }
+        }
+      }
+      log(addLyW)
+    }
 
-    // // log all layers & filtre MSShape
+  }
+
+// // log all layers & filtre MSShape
     // log(layers.length)
     // for (var l = 0; l < layers.count(); l++) {
     //   var layer = layers[l];
@@ -35,29 +65,3 @@ var onRun = function(context) {
     //     log(layers[l])
     //   }
     // }
-
-    // log slices :
-    for(var k = 0; k < layers.count(); k++){
-      var layer = layers[k];
-      if(layer.class() == "MSSliceLayer"){
-        //do something
-        // log( layers[k] )
-        log( layer )
-        // log( layers[k].frame() )
-        var layClass = layer.frame()
-        var layW = layClass.width()
-        var layY = layClass.y()
-        log( `w: ${layW} y:${layY} ` )
-        if (layW === 620){
-          log(`layer: ${layer} vaut une slice `)
-        }else if(layW <= 620){
-          log(`layer: ${layer} est à additionner pour arriver jusqu'à 620 `)
-          // while(layW !== 620){
-          // }
-        }
-      }
-
-    }
-  }
-
-}
