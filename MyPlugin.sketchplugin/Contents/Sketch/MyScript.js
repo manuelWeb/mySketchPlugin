c12f9239b26b9171a635c16d7dc8dbb4277f704b
manuelWeb <devries.webdesigner@gmail.com>
2018-06-13 20:50:39 +0200

    add obj

--- MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
+++ MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
@@ -1,82 +1,73 @@
 
 var onRun = function(context) {
   var doc = context.document;
-  var pages = [doc pages];
-  var p0 = pages[1];
-  var artboard = [p0 artboards];
-  function showObj(obj) {
-    return {...obj}
-  }
-  log(artboard[0])
-  log(showObj(artboard[0]))
-  // log(`[doc pages][1]: ${pages[1]}`)
+  // var pages = [doc pages];
+  // var p0 = pages[1];
+  // var artboard = [p0 artboards];
+  // var art0 = artboard[0];
+  // var lay = [art0 layers]
+  // function showObj(obj) {
+  //   return {...obj}
+  // }
+  // log(artboard[0])
+  // log(lay)
   var selections = context.selection;
-  if(selections.count() === 0){
-    doc.showMessage("Please select slices.")
-  }else{
-    var y = [];
-    for(var i = 0; i < selections.count(); i++) {
-      var selection = selections[i];
-      y.push( selection.frame().y() )
-
-      if (selection.class() != "MSSliceLayer") {
-        doc.showMessage( `Please select slices. ${[selection name]} is not a slice` )
-      }else{
-        // log(`name: ${[selection name]} width: ${selection.frame().width()}/height: ${selection.frame().height()}`)
-        // log(`x: ${selection.frame().x()}/y: ${selection.frame().y()}`)
-      }
-    }
-    log(`y:${y}`);
-    // log(`y.filter(): ${y.filter(function(item) { return typeof item == 'number'; })}`)
-    let onlyUnique = (value, index, self) => self.indexOf(value) === index;
-    // log(`y.filter(): ${y.filter((item) => typeof item == item)}`)
-    log(`y.filter(): ${y.filter(onlyUnique)}`)
-    // if(selection.class() == "MSSliceLayer"){
-    //   var layName = [selection name]
-    //   var layY = layClass.y()
-    //   log( `MSSliceLayer: ${layName} w:${layW} y:${layY} ` )
+  var test = selections[0]
+  var testName = [test name]
+  const slice1 = test.frame().width();
+  doc.showMessage(`selections.count(): ${selections.count()}`)
+  log(typeof testName)
+  log(typeof slice1)
+  
+  // var obj = {a:1, b:2, c:3};
+  // var obj = context.document.selectedLayers().layers();
+  // for (var prop in obj) {
+    //   log(`obj.${prop} = ${obj[prop]}`);
     // }
+  var createObj = function (a,b,c,d,e) {
+    return {
+      'name': a,
+      'width': b
+    }
   }
-  // for (var i = 0; i < pages.count(); i++){
-  //   var page = pages[i];
-  //   // log([page name])
-  //   var artboards = [page artboards];
-  //   // log(artboards)
+  var selection = context.selection
+  for (var i = 0; i < selection.count(); i++) {
+    const layer  = selection[i],
+          name   = layer.name,
+          width  = layer.frame().width(),
+          height = layer.frame().height(),
+          x      = layer.frame().x(),
+          y      = layer.frame().y();
+    // log('layer ' + layer.name + ' is selected.')
+    // log('layerWidth ' + layer.frame().width() + ' is selected.')
+    createObj(name,width,height,x,y)
+  }
+  log(createObj)
+  // for(var key in selection) {
+  //   if(myObject.hasOwnProperty(key)) {
+  //     log(myObject[key])
+  //   }
+  // }
 
-  //   for (var z = 0; z < artboards.count(); z++){
-  //     var artboard = artboards[z];
-  //     var layers = [artboard layers];
-  //   } // for z
+  // if(selections.count() === 0){
+  //   doc.showMessage("Please select slices.")
+  // }else{
+  //   var y = [];
+  //   for(var i = 0; i < selections.count(); i++) {
+  //     var selection = selections[i];
+  //     y.push( selection.frame().y() )
 
-  //   // log slices :
-  //   var arySlice = []
-  //   var sum = 0;
-  //   for(var k = 0; k < layers.count(); k++){
-  //     var layer = layers[k];
-  //     if(layer.class() == "MSSliceLayer"){
-  //       var layName = [layer name]
-  //       var layClass = layer.frame()
-  //       var layW = layClass.width()
-  //       var layY = layClass.y()
-  //       if (layW === 620){
-  //         log(`layer: ${layName} vaut une slice `)
-  //         arySlice.push(layName)
-  //       }else if(layW <= 620){
-  //         log( `layer: ${layName} w:${layW} y:${layY} ` )
-  //         sum += layW
-  //       }
-  //       log(`sum: ${sum}`)
+  //     if (selection.class() != "MSSliceLayer") {
+  //       doc.showMessage( `Please select slices. ${[selection name]} is not a slice` )
+  //     }else{
+  //       // log(`name: ${[selection name]} width: ${selection.frame().width()}/height: ${selection.frame().height()}`)
+  //       // log(`x: ${selection.frame().x()}/y: ${selection.frame().y()}`)
   //     }
-  //     // while (sum <= 620) {
-  //       //   continue
-  //       // }
-  //   } // for k
-  //   log(arySlice)
-  // } // for 1
+  //   }
+  //   log(`y:${y}`);
+  //   let onlyUnique = (value, index, self) => self.indexOf(value) === index;
+  //   log(`y.filter(): ${y.filter(onlyUnique)}`)
 
-} // onRun
+  // }
 
-
-
-  // var doc = context.document;
-  // log(`context.document: ${context.document}`)
+}
