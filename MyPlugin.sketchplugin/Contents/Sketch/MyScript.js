afe778260e05750e50a23d75c4dc937e424f4cc7
manuelWeb <devries.webdesigner@gmail.com>
2018-06-16 15:51:53 +0200

    create object with above obj

--- MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
+++ MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
@@ -1,77 +1,53 @@
-
+// "layer", "name", "width", "height", "x", "y"
 var onRun = function(context) {
   var doc = context.document;
-  // var pages = [doc pages];
-  // var p0 = pages[1];
-  // var artboard = [p0 artboards];
-  // var art0 = artboard[0];
-  // var lay = [art0 layers]
-  // function showObj(obj) {
-  //   return {...obj}
-  // }
-  // log(artboard[0])
-  // log(lay)
   var selections = context.selection;
-  var test = selections[0]
-  var testName = [test name]
-  const slice1 = test.frame().width();
-  doc.showMessage(`selections.count(): ${selections.count()}`)
-  log(typeof testName)
-  log(typeof slice1)
-  
-  // var obj = {a:1, b:2, c:3};
-  // var obj = context.document.selectedLayers().layers();
-  // for (var prop in obj) {
-    //   log(`obj.${prop} = ${obj[prop]}`);
-    // }
-  var sliceLayout = ["layer", "name", "width", "height", "x", "y"]
-  class Slice {
-
-    constructor(attribut, value) {
-      this.attribut = attribut;
-      this.value = value;
-    }
-    present() {
-      // log(`attribut: ${this.attribut} value: ${this.value} `)
-      return {
-        "attribut": this.attribut,
-        "value": this.value
-      }
-    }
-
+  function alert(title, message){
+    var app = [NSApplication sharedApplication];
+    [app displayDialog:message withTitle:title];
   }
-  let width = new Slice("width", test.frame().width());
-  let height = new Slice("height", test.frame().height());
-  // john.present();  width.present(): ${width.present()}
-  log(width.present())
-  log(`width.attr: ${width.attribut}, width.value: ${height.value}, height.attr: ${height.attribut}, height.value: ${height.value} `)
-  log(`test.frame().x():${test.frame().x()},test.name():${test.name()},`)
+  !selections.count() ? doc.showMessage(`Please select slice`) : doc.showMessage(` your selections: ${selections}`)
+  // !selections.count() ? alert(`Please select slice`) : alert(` your selections: ${selections}`)
 
-  // for(var key in selection) {
-  //   if(myObject.hasOwnProperty(key)) {
-  //     log(myObject[key])
-  //   }
-  // }
+  class Slice_name {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  class Cel {
+    constructor(cel_item) {
+      this.cel_item = cel_item;
+    }
+  }
+  class Slice_size {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+  // let s1 = new Slice(test.frame().height());
+  // let s1Name = new Slice_name( test.name() );
+  // if (test[0].frame().x() === test[1].frame().x())
+  // let s1Size = new Slice_size( test.frame().width(), test.frame().height() );
+  // créer une fonction qui fabrique les objets slices :
+  // function creatObjSlice(selections){
+    var objS = {}
+    var cpt = 1;
+    for(let i = 0; i < selections.count(); i++) {
+      var selSlice = selections[i]
+      // log(selections[i])
+      // log(selSlice.frame().width())
+      if (selSlice.sketchObject.className() == "MSSliceInstance") {
+        var curName = selSlice.name()
+      }
+      // alert(curName)
+      objS["item_"+cpt] += curName
+      cpt++
+      // objS["item"]["width"] += selSlice.frame().width()
+      // let monObj = new Slice( selSlice.frame().height() );
+    }
+    log(objS.item_1)
+    // log(objS.item_1)
 
-  // if(selections.count() === 0){
-  //   doc.showMessage("Please select slices.")
-  // }else{
-  //   var y = [];
-  //   for(var i = 0; i < selections.count(); i++) {
-  //     var selection = selections[i];
-  //     y.push( selection.frame().y() )
-
-  //     if (selection.class() != "MSSliceLayer") {
-  //       doc.showMessage( `Please select slices. ${[selection name]} is not a slice` )
-  //     }else{
-  //       // log(`name: ${[selection name]} width: ${selection.frame().width()}/height: ${selection.frame().height()}`)
-  //       // log(`x: ${selection.frame().x()}/y: ${selection.frame().y()}`)
-  //     }
-  //   }
-  //   log(`y:${y}`);
-  //   let onlyUnique = (value, index, self) => self.indexOf(value) === index;
-  //   log(`y.filter(): ${y.filter(onlyUnique)}`)
-
-  // }
 
 }
