214a4d31bcf2374a262733a183ab6febefe99b79
manuelWeb <devries.webdesigner@gmail.com>
2018-06-19 20:03:49 +0200

    add fct constructeur d'objet Cellules avec attrib : name w h x y

--- MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
+++ MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
@@ -18,19 +18,43 @@ var onRun = function(context) {
     const objCel = {}
     const aryS = []
     let cpt = 1;
+    let allTd = {};
+    function Cellules(name,width,height,x,y) {
+      this.name = name
+      this.width = width
+      this.height = height
+      this.x = x
+      this.y = y
+    }
+    // Cellules.prototype.pushTd = function () {
+    //   return this.name
+    // }
     for(let i = 0; i < selections.count(); i++) {
       const selSlice = selections[i]
       const curName = selSlice.name()
-
-      objCel["td_"+cpt]           = curName;
+      const objCel_ = new Cellules(
+        curName,
+        selSlice.frame().width(),
+        selSlice.frame().height(),
+        selSlice.frame().x(),
+        selSlice.frame().y()
+      )
+      
+      objCel["td_"+cpt]           = curName
       objCel["td_"+cpt]["width"]  = selSlice.frame().width()
       objCel["td_"+cpt]["height"] = selSlice.frame().height()
       objCel["td_"+cpt]["x"]      = selSlice.frame().x()
       objCel["td_"+cpt]["y"]      = selSlice.frame().y()
-
+      
+      allTd["td_"+cpt] = objCel_;
       cpt++
     }
-
+    // log(`allTd.td_1:${allTd.td_1} `)
+    for (const prop in allTd) {
+      log(`prop: ${prop} `)
+      log(allTd[prop])
+    }
+    // log(allTd)
     var acc = 0
     for (const prop in objCel) {
       // log(`objCel.${prop} = ${objCel[prop]}, y:${objCel[prop]["y"]} `)
