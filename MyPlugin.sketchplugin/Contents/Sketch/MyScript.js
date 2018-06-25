27a6215ec8bda143ad97a720a8564d0a693e0188
manuelWeb <devries.webdesigner@gmail.com>
2018-06-25 21:15:52 +0200

    change var to const

--- MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
+++ MyPlugin.sketchplugin/Contents/Sketch/MyScript.js
@@ -3,21 +3,20 @@
 var UI = require('sketch/ui')
 var onRun = function(context) {
   // const width_prompt = UI.getStringFromUser("Width of your masterpiece", 620)
-  const doc = context.document
+  const doc        = context.document
   const selections = context.selection
-  var pages = [doc pages];
-  var artwidth = 0;
-  for (var i = 0; i < pages.count(); i++)
+  const pages      = [doc pages];
+  let artwidth     = 0;
+  for (let i = 0; i < pages.count(); i++)
   {
-      var page = pages[i];
-      var artboards = [page artboards];
+      const page = pages[i];
+      const artboards = [page artboards];
       // 0 est artboard le plus bas dans la liste
       artwidth = artboards[0].frame().width()
       log(artwidth)
   }
 
-// also exposed on Document
-
+  // also exposed on Document
   !selections.count() ? doc.showMessage(`Please select slice`) : doc.showMessage(` your selections: ${selections}`)
 
   let cpt = 1;
@@ -25,11 +24,11 @@ var onRun = function(context) {
 
   class Cellules{
     constructor(name,width,height,x,y){
-      this.name = name
-      this.width = width
+      this.name   = name
+      this.width  = width
       this.height = height
-      this.x = x
-      this.y = y
+      this.x      = x
+      this.y      = y
     }
   }
 
@@ -49,16 +48,16 @@ var onRun = function(context) {
   }
 
   let accWidth = 0
-  let objTemp = {}
+  let objTemp  = {}
   let cptS     = 1
 
   let sliceComplet = {}
 
   function isSlice(idxTD, td) {
-    var w = td.width;
-    var x = td.x;
-    var y = td.y;
-    var sliceNum = "slice_" + cptS
+    const w = td.width;
+    const x = td.x;
+    const y = td.y;
+    const sliceNum = "slice_" + cptS
     switch (true) {
       // slice avec une td
       case (w === artwidth || accWidth === artwidth):
@@ -66,12 +65,9 @@ var onRun = function(context) {
         ;({ accWidth, objTemp, cptS } = noNestedTab(accWidth, w, objTemp, idxTD, td, artwidth, sliceComplet, sliceNum, cptS));
       break;
 
-      // slice avec plusieurs td mais pas d'imbrication
+      // slice avec plusieurs td mais sans d'imbrication
       case (w < artwidth):
         // log(`td: ${td.name} x-> ${x} `);
-        // if( objTemp[`td_${idxTD-1}`] ){
-        //   log(objTemp[`td_${idxTD-1}`])
-        // }
         ;({ accWidth, objTemp, cptS } = noNestedTab(accWidth, w, objTemp, idxTD, td, artwidth, sliceComplet, sliceNum, cptS));
       break;
       // default:
@@ -96,10 +92,6 @@ function noNestedTab(accWidth, w, objTemp, idxTD, td, artwidth, sliceComplet, sl
   //   log('objTemp[td_'+idxTD+'] -> ');log(objTemp[`td_${idxTD}`]);
   // }
 
-  // if(td.width === artwidth) {
-  //   sliceComplet[sliceNum] = objTemp;
-  //   objTemp = {};
-  // }
   if (accWidth === artwidth) {
     sliceComplet[sliceNum] = objTemp;
     objTemp = {};
