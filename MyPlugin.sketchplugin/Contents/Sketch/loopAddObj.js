9c6c160d0f29ddc6c8597c67152a389742d86ef5
manuelWeb <devries.webdesigner@gmail.com>
2018-07-28 12:35:06 +0200

    comment programmation imperative -> to test with functional programming

--- MyPlugin.sketchplugin/Contents/Sketch/loopAddObj.js
+++ MyPlugin.sketchplugin/Contents/Sketch/loopAddObj.js
@@ -54,85 +54,85 @@ const objAllTD ={
     }
   }
 
-let accWidth     = 0
-let keys         = Object.keys(objAllTD);
-let numtd        = 1
-let num          = 1
-let objSlice     = {}
-let nestedObj    = {}
+// let accWidth     = 0
+// let keys         = Object.keys(objAllTD);
+// let numtd        = 1
+// let num          = 1
+// let objSlice     = {}
+// let nestedObj    = {}
 
-for(let i = 0; i < keys.length; i++) {
+// for(let i = 0; i < keys.length; i++) {
 
-  const current  = keys[i]
-  const previous = keys[i - 1]
-  const next     = keys[i + 1]
+//   const current  = keys[i]
+//   const previous = keys[i - 1]
+//   const next     = keys[i + 1]
 
-  if(objAllTD[current].width === 620){
-    console.log(`slice avec une TD: ` + objAllTD[current].name)
-    objSlice[ 'slice_' + num ] = {['td_'+numtd]: objAllTD[current].name}
-    accWidth = 0
-    num++
-  }
+//   if(objAllTD[current].width === 620){
+//     console.log(`slice avec une TD: ` + objAllTD[current].name)
+//     objSlice[ 'slice_' + num ] = {['td_'+numtd]: objAllTD[current].name}
+//     accWidth = 0
+//     num++
+//   }
 
-  // un bug ici si pas de next alors echou résolu par le else utile uniquement si nested table est en derniere position
-  if( objAllTD.hasOwnProperty(next) ){
+//   // un bug ici si pas de next alors echou résolu par le else utile uniquement si nested table est en derniere position
+//   if( objAllTD.hasOwnProperty(next) ){
 
-    if(objAllTD[current].x === objAllTD[next].x
-      && objAllTD[current].width === objAllTD[next].width){
+//     if(objAllTD[current].x === objAllTD[next].x
+//       && objAllTD[current].width === objAllTD[next].width){
 
-      if(!objSlice[ 'slice_' + num ]){
-        console.log('slice_'+num);
-      }
+//       if(!objSlice[ 'slice_' + num ]){
+//         console.log('slice_'+num);
+//       }
 
-      nestedObj['td_'+parseInt(numtd)] = objAllTD[current]
-      nestedObj['td_'+parseInt(numtd+1)] = objAllTD[next]
+//       nestedObj['td_'+parseInt(numtd)] = objAllTD[current]
+//       nestedObj['td_'+parseInt(numtd+1)] = objAllTD[next]
 
-      console.log('nestedObj : ',nestedObj);
-      console.log('doit on ajouter : '+nestedObj['td_'+parseInt(numtd+1)].name);
+//       console.log('nestedObj : ',nestedObj);
+//       console.log('doit on ajouter : '+nestedObj['td_'+parseInt(numtd+1)].name);
 
-      // le BUG se trouve ici ->
-      numtd++
-    }
+//       // le BUG se trouve ici ->
+//       numtd++
+//     }
 
-    if(objAllTD[next].name){
-      console.log(`objAllTD[next].name:.......`+objAllTD[next].name);
-      if(nestedObj){
-        console.log(`nestedObj['td_'+parseInt(numtd)]:+++++`,nestedObj['td_'+parseInt(numtd)]);
-      }
-    }
+//     if(objAllTD[next].name){
+//       console.log(`objAllTD[next].name:.......`+objAllTD[next].name);
+//       if(nestedObj){
+//         console.log(`nestedObj['td_'+parseInt(numtd)]:+++++`,nestedObj['td_'+parseInt(numtd)]);
+//       }
+//     }
 
-    // si nestedObj have td with y that === objAllTD[current].y
-    // recup des TD qui suive nested table
-    nestedObj.td_1 ? nestedObj.td_1.y === objAllTD[current].y && nestedObj.td_1.name !== objAllTD[current].name ?  addNest() : console.log('nestedObj.td_1.y !== objAllTD[current].y') : console.log('!nestedObj.td_1');
+//     // si nestedObj have td with y that === objAllTD[current].y
+//     // recup des TD qui suive nested table
+//     nestedObj.td_1 ? nestedObj.td_1.y === objAllTD[current].y && nestedObj.td_1.name !== objAllTD[current].name ?  addNest() : console.log('nestedObj.td_1.y !== objAllTD[current].y') : console.log('!nestedObj.td_1');
 
-    function addNest() {
-      nestedObj['td_'+parseInt(numtd)] = objAllTD[current]
-      numtd++;
-    }
+//     function addNest() {
+//       nestedObj['td_'+parseInt(numtd)] = objAllTD[current]
+//       numtd++;
+//     }
 
-  }
+//   }
 
-  if( objAllTD[current].width < 620 && accWidth < 620 ){
-    accWidth += objAllTD[current].width
-    // éviter la mutabilité de slice_num
-    if(!objSlice[ 'slice_' + num ]){
-      objSlice[ 'slice_' + num ] = {['td_'+numtd]: objAllTD[current].name}
-      // console.log('pas de ssobj on le creer: ',objSlice[ 'slice_' + num ]);
-    }else{
-      objSlice[ 'slice_' + num ]['td_'+numtd] = objAllTD[current].name
-      // console.log('ssobj ok on rempli',objSlice[ 'slice_' + num ]['td_'+numtd]);
-    }
-    numtd++
-    if ( accWidth === 620 ) {
-      console.log(`une slice complet td derniere: ${objAllTD[current].name} `)
-      accWidth = 0
-      numtd = 1
-      num++
-    }
-  }
+//   if( objAllTD[current].width < 620 && accWidth < 620 ){
+//     accWidth += objAllTD[current].width
+//     // éviter la mutabilité de slice_num
+//     if(!objSlice[ 'slice_' + num ]){
+//       objSlice[ 'slice_' + num ] = {['td_'+numtd]: objAllTD[current].name}
+//       // console.log('pas de ssobj on le creer: ',objSlice[ 'slice_' + num ]);
+//     }else{
+//       objSlice[ 'slice_' + num ]['td_'+numtd] = objAllTD[current].name
+//       // console.log('ssobj ok on rempli',objSlice[ 'slice_' + num ]['td_'+numtd]);
+//     }
+//     numtd++
+//     if ( accWidth === 620 ) {
+//       console.log(`une slice complet td derniere: ${objAllTD[current].name} `)
+//       accWidth = 0
+//       numtd = 1
+//       num++
+//     }
+//   }
 
-}
+// }
 
-console.log(nestedObj)
-console.log(objSlice)
+// console.log(nestedObj)
+// console.log(objSlice)
 
